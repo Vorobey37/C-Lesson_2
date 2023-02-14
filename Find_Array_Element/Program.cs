4ae5469952a4e8476e70fdf56a69ff1978b05649
Abvorobey <abvorobey@gmail.com>
2023-02-14 16:25:04 +0300

    Базовый пример поиска элемента

---
+++ Find_Array_Element/Program.cs
@@ -0,0 +1,15 @@
+﻿int[]array={1, 23, 32, 59, 68, 8, 9, 15};
+int n = array.Length;
+int find = 9;
+int index = 0;
+while (index<n)
+{
+if (array[index] == find)
+{Console.WriteLine(index);
+return;}
+
+    index++;
+}
+Console.WriteLine("Не нашел(((");
+
+
